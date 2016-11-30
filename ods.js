1cdbd5798538f7106b70eb0ba6260926362acc45
Cyprian Gracz <cyprian.gracz@micro-jumbo.eu>
2016-11-30 14:17:22 +0100

    fix

--- ods.js
+++ ods.js
@@ -6,13 +6,6 @@ var ODS = {};
 /* Open Document Format for Office Applications (OpenDocument) Version 1.2 */
 var get_utils = function() {
 	if(typeof XLSX !== 'undefined') return XLSX.utils;
-	if(typeof module !== "undefined" && typeof require !== 'undefined') try {
-		return require('../xlsx').utils;
-	} catch(e) {
-		try { return require('./xlsx').utils; }
-		catch(ee) { return require('xlsx').utils; }
-	}
-	throw new Error("Cannot find XLSX utils");
 };
 var has_buf = (typeof Buffer !== 'undefined');
 
